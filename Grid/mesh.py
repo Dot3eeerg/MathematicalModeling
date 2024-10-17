85b1eeb8e121e0d794f48fff517a0a25e3d062da
Ivan Danchenko <danchenko.ivan@inbox.ru>
2024-10-17 17:56:30 +0700

    Grid sample

--- Grid/mesh.py
+++ Grid/mesh.py
@@ -46,25 +46,24 @@ def read_neumann_from_file(filename):
     return neumann
 
 # Function to visualize the grid with rectangular elements and connections between points
-def plot_mesh_with_elements(points, elements = None, dirichlet = None, neumann = None):
+def plot_mesh_with_elements(points, elements, dirichlet = None, neumann = None):
     # Create the plot and axes
     fig, ax = plt.subplots(figsize=(10, 6))
 
     # Loop through each element
-    if elements is not None:
-        for element in elements:
-            if len(element) == 5:
-                # Create a polygon for the rectangular element using its vertices
-                vertices = [points[element[0]],
-                            points[element[1]],
-                            points[element[2]],
-                            points[element[3]]]
-                # Random color for each element
-                colorElem = ("cyan", "green", "grey", "blue")
+    for element in elements:
+        if len(element) == 5:
+            # Create a polygon for the rectangular element using its vertices
+            vertices = [points[element[0]],
+                        points[element[1]],
+                        points[element[2]],
+                        points[element[3]]]
+            # Random color for each element
+            colorElem = ("cyan", "green", "grey", "blue")
 
-                # Create a polygon and add it to the plot
-                polygon = Polygon(vertices, edgecolor='black', facecolor=colorElem[element[4]], alpha=0.5)
-                ax.add_patch(polygon)
+            # Create a polygon and add it to the plot
+            polygon = Polygon(vertices, edgecolor='black', facecolor=colorElem[element[4]], alpha=0.5)
+            ax.add_patch(polygon)
 
     # Separate x and y coordinates
     x_coords, y_coords = zip(*points)
@@ -75,7 +74,7 @@ def plot_mesh_with_elements(points, elements = None, dirichlet = None, neumann =
     if dirichlet is not None:
         dirichlet_coords = [points[i] for i in dirichlet]
         dx, dy = zip(*dirichlet_coords)
-        ax.scatter(dx, dy, color='green', marker='o', label="Узлы с первым краевым условием")
+        ax.scatter(dx, dy, color='blue', marker='o', label="Узлы с первым краевым условием")
 
     if neumann is not None:
         neumann_label_added = False
@@ -93,11 +92,11 @@ def plot_mesh_with_elements(points, elements = None, dirichlet = None, neumann =
 
 # Reading the mesh data and elements from files
 mesh_points = read_mesh_from_file("points")  # Replace "mesh.txt" with your mesh file path
-# elements = read_elements_from_file("elements")  # Replace "elements.txt" with your elements file path
-# dirichlet = read_dirichlet_from_file("dirichlet")
-# neumann = read_neumann_from_file("neumann")
+elements = read_elements_from_file("finite_elements")  # Replace "elements.txt" with your elements file path
+dirichlet = read_dirichlet_from_file("dirichlet")
+neumann = read_neumann_from_file("neumann")
 
 # Plotting the mesh grid with rectangular elements and connections
 #plot_mesh_with_elements(mesh_points, elements, dirichlet, neumann)
 # plot_mesh_with_elements(mesh_points, elements, neumann = neumann)
-plot_mesh_with_elements(mesh_points)
+plot_mesh_with_elements(mesh_points, elements, dirichlet, neumann)
